fda73aaf2ab5f50d82194ed4e0156aa1aa0843a4
ericcchiu <eric.chn.chiu@gmail.com>
2020-07-07 09:21:43 -0500

    (FEATURE) Added Tool struct

--- tools/model.go
+++ tools/model.go
@@ -1,3 +1,15 @@
+package tools
 
+import "time"
 
+// Tool creates the model that describes the attributes of a tool
 
+type Tool struct {
+	ID          string    `json:"id" db:"id"`
+	Name        string    `json:"name" db:"name"`
+	Description string    `json:"description" db:"description"`
+	Price       float64   `json:"price" db:"price"`
+	Quantity    int       `json:"quantity" db:"quantity"`
+	Created     time.Time `json:"created" db:"created"`
+	Updated     time.Time `json:"updated" db:"updated"`
+}
