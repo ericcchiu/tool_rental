c63083e9c8fd80eb30384c5594719fd495ed474e
ericcchiu <eric.chn.chiu@gmail.com>
2020-07-07 18:20:58 -0500

    (FEATURE) Add NewPostgresToolDataStore function

--- db/psql/tools.go
+++ db/psql/tools.go
@@ -1,17 +1,95 @@
 package psql
-
+​
 import (
 	"database/sql"
-
-	_ "github.com/lib/pq"
+	"fmt"
+	"time"
+​
+	"github.homedepot.com/emc4jq2/tool_rental/tools" //replace with yourldap with your ldap
+​
+	_ "github.com/lib/pq" //gives access to the PSQL driver
 )
-
-type toolDataStore struct {
+​
+type toolDataStore struct { //1
 	db *sql.DB
 }
-
-func NewPostgresToolDataStore(db *sql.DB) *toolDataStore {
+​
+func NewPostgresToolDataStore(db *sql.DB) *toolDataStore { //2
 	return &toolDataStore{
 		db,
 	}
 }
+​
+func (t toolDataStore) Create(tool *tools.Tool) (err error) {
+	sqlStatement := `INSERT INTO tools(name, description, price, quantity, created_at, update_at)
+					 VALUES ($1, $2, $3, $4, $5, $6) RETURNING id` //1
+	row := t.db.QueryRow(sqlStatement, tool.Name, tool.Description, tool.Price, tool.Quantity, time.Now(), time.Now()) //2
+	err = row.Scan(&tool.ID)
+	if err == nil {
+		fmt.Println("New record ID is:", tool.ID) //3
+	}
+​
+	return
+}
+​
+func (t toolDataStore) FindByID(id string) (tool *tools.Tool, err error) {
+	tool = new(tools.Tool)
+	sqlStatement := `SELECT * FROM tools WHERE id=$1`
+	row := t.db.QueryRow(sqlStatement, id)                                                                             //1
+	err = row.Scan(&tool.ID, &tool.Name, &tool.Description, &tool.Price, &tool.Quantity, &tool.Created, &tool.Updated) //2
+​
+	switch err {
+	case sql.ErrNoRows: //3
+		return tool, fmt.Errorf("no rows were returned")
+	case nil:
+		return
+	default:
+		return
+	}
+}
+​
+func (t toolDataStore) FindAll() (allTools []*tools.Tool, err error) {
+	sqlStatement := `SELECT * FROM tools`
+	rows, err := t.db.Query(sqlStatement) //1
+	defer rows.Close()                    //2
+	for rows.Next() {                     //3
+		currentTool := new(tools.Tool)
+		err = rows.Scan(&currentTool.ID, &currentTool.Name, &currentTool.Description, &currentTool.Price, &currentTool.Quantity, &currentTool.Created, &currentTool.Updated)
+		if err == nil {
+			allTools = append(allTools, currentTool) //append only if there was not an error
+		}
+	}
+​
+	err = rows.Err() //4
+	return
+}
+​
+func (t toolDataStore) Update(tool *tools.Tool) (err error) {
+	_, err = t.FindByID(tool.ID) //Checks to see if ID is provided/valid
+	if err != nil {
+		return
+	}
+	sqlStatement := `UPDATE tools
+					SET name = $2, description = $3, price = $4, quantity = $5, update_at = $6
+					WHERE id = $1;`
+	result, err := t.db.Exec(sqlStatement, tool.ID, tool.Name, tool.Description, tool.Price, tool.Quantity, time.Now())
+	fmt.Printf("%v row affected\n", result)
+	return err
+}
+​
+func (t toolDataStore) Delete(id string) (err error) {
+	_, err = t.FindByID(id)
+	if err != nil {
+		return fmt.Errorf("delete: id not found")
+	}
+	sqlStatement := `DELETE FROM tools
+					WHERE id = $1;`
+	result, err := t.db.Exec(sqlStatement, id)
+	numRows, _ := result.RowsAffected()
+	fmt.Printf("%v rows deleted", numRows)
+	return err
+}
+
+
+
+
--- main.go
+++ main.go
@@ -11,14 +11,16 @@ import (
 )
 
 func main() {
-	// Load environmental variables
+	var toolDataStore tools.ToolDataStore
+	// NewPostgresToolDataStore
+	// ------- Load environmental variables-----------
 	err := godotenv.Load()
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal("Error loading .env file")
 	}
 
-	var toolDataStore tools.ToolDataStore
-	// ------- Credentials -----------
+	// ------- Credentials ---------------------------
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("PORT")
 	user := os.Getenv("DB_USERNAME")
@@ -26,22 +28,26 @@ func main() {
 	dbname := os.Getenv("DB_NAME")
 	sslmode := os.Getenv("SSL_MODE")
 
-	fmt.Printf("WHAT IS THIS user: %s \n", user)
-	// fmt.Printf(os.Getenv("DB_HOST"))
+	// fmt.Printf("WHAT IS THIS user: %s \n", host)
+	// fmt.Printf("WHAT IS THIS port: %s \n", port)
+	// fmt.Printf("WHAT IS THIS user: %s \n", user)
+	// fmt.Printf("WHAT IS THIS password: %s \n", password)
+	// fmt.Printf("WHAT IS THIS dbname: %s \n", dbname)
 
-	// ------ Create Connection ------
+	// ------ Create Connection -----------------------
 	connection := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", user, password, host, port, dbname, sslmode)
 
 	db, err := psql.NewPostgresConnection(connection)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-
 	defer db.Close()
 
-	// toolDataStore = "firefly" //Data store constructor implemented here
+	fmt.Println(toolDataStore)
+	toolDataStore = psql.NewPostgresToolDataStore(db)
 
 	// toolService := tools.NewToolService(toolDataStore)
 
-	fmt.Println(toolDataStore)
+	// fmt.Println(toolDataStore)
 }
