c14c1262ad5decaccb5d9bd2e4c3f0aee4de3764
ericcchiu <eric.chn.chiu@gmail.com>
2020-07-08 15:25:32 -0500

    (FEATURE) Add Update and Delete method

--- db/psql/tools.go
+++ db/psql/tools.go
@@ -19,19 +19,21 @@ func NewPostgresToolDataStore(db *sql.DB) *toolDataStore {
 	}
 }
 
+// Create method takes in a Tool instance and places the new tool into the database for the toolDataStore struct
 func (t toolDataStore) Create(tool *tools.Tool) (err error) {
 	sqlStatement := `INSERT INTO tools(name, description, price, quantity, created_at, update_at)
-					 VALUES ($1, $2, $3, $4, $5, $6) RETURNING id` //1
+					 VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`
 	row := t.db.QueryRow(sqlStatement, tool.Name, tool.Description, tool.Price, tool.Quantity, time.Now(), time.Now())
 	err = row.Scan(&tool.ID)
 
 	if err == nil {
-		fmt.Println("New record ID is:", tool.ID) //3
+		fmt.Println("New record ID is:", tool.ID)
 	}
 
 	return
 }
 
+// FindByID method takes in a Tool ID and returns the associated tool, if it exists, for the toolDataStore struct.
 func (t toolDataStore) FindByID(id string) (tool *tools.Tool, err error) {
 	tool = new(tools.Tool)
 	sqlStatement := `SELECT * FROM tools WHERE id=$1`
@@ -48,6 +50,7 @@ func (t toolDataStore) FindByID(id string) (tool *tools.Tool, err error) {
 	}
 }
 
+// FindAll method returns all the number of rows for the toolDataStore struct
 func (t toolDataStore) FindAll() (allTools []*tools.Tool, err error) {
 	sqlStatement := `SELECT * FROM tools`
 	rows, err := t.db.Query(sqlStatement)
@@ -64,6 +67,7 @@ func (t toolDataStore) FindAll() (allTools []*tools.Tool, err error) {
 	return
 }
 
+// Update method takes in a Tool instance and updates the tool with the corresponding ID in the database for the toolDataStore struct.
 func (t toolDataStore) Update(tool *tools.Tool) (err error) {
 	_, err = t.FindByID(tool.ID) //Checks to see if ID is provided/valid
 	if err != nil {
@@ -77,6 +81,7 @@ func (t toolDataStore) Update(tool *tools.Tool) (err error) {
 	return err
 }
 
+// Delete method takes in a Tool ID and deletes the tool with the corresponding ID in the database for the toolDataStore struct
 func (t toolDataStore) Delete(id string) (err error) {
 	_, err = t.FindByID(id)
 	if err != nil {
